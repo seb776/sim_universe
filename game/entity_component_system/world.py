0a1e35d5f8a88b84f384eac22ee6bd2e83a4c275
seb776 <sebastien.maire@epitech.eu>
2015-01-18 20:42:33 +0100

    update

--- game/entity_component_system/world.py
+++ game/entity_component_system/world.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python2
 
-import entity_manager
-
 class World:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs) # to allow user to store data
-        self._entities = []
-        self._refreshed = []
-        self._removed = []
-        self.entity_manager = entity_manager.EntityManager(self)
-        self.system_manager = system_manager.SystemManager(self)
-
-    def loop_start(self):
-        # to actualize refreshed and removed entities
-        pass
+    def __init__(self):
+        self.entity_manager = EntityManager()
+        self.system_manager = SystemManager()
+        self.group_manager = GroupManager()
+        self.tag_manager = TagManager()
